8f80aa32d8185c44ca02ef4d58750ad1a00f7a6e
eric <contreras.eric18@gmail.com>
2021-10-12 02:17:00 -0400

    commit tree b8426fd26f1ba12b1f6d6c7e2912c3b054eecc87

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,25 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends: []}, action){
+    switch(action.type){
+        default: 
+        return state;
+        case "ADD_FRIEND":
+            return(
+                {...state,
+                friends:[
+                    ...state.friends, 
+                    action.friend
+                    ]
+                }
+            )
+        case "REMOVE_FRIEND":
+            const toRemove = state.friends.findIndex(friend => friend.id === action.id);
+            return(
+                {...state, 
+                    friends: [
+                        ...state.friends.slice(0, toRemove),
+                        ...state.friends.slice(toRemove +1)
+                    ]
+                }
+            )
+    }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,8 @@
 export function managePresents(state, action){
+    switch(action.type) {
+        default:
+            return {numberOfPresents: 0}
+        case "INCREASE":
+            return { numberOfPresents: state.numberOfPresents + 1 };
+    }
 }
